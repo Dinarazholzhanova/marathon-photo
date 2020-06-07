d29f51736b2cdb8dfccd5b4c7e3b4dc327b37c08
DilnurAbu <dilnur.abu@gmail.com>
2020-06-07 14:04:18 +0600

    added photos

--- marathon/settings.py
+++ marathon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,16 +79,6 @@ WSGI_APPLICATION = 'marathon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'marathon',
-        'USER': 'postgres',
-        'PASSWORD': 'dinara',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 
 
 # Password validation
